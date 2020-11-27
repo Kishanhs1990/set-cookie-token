375a8ebe191329e5919207c60ddfa2583aed5571
Kishan <kishanhs1990@gmail.com>
2020-11-27 14:38:25 +0530

    poc to show set token to cookie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import getTokenFromCookie from './GetCookie';
+import setTokenToCookie from './SetCookie';
+import React, { useEffect } from 'react';
+
+//token will be injected from env
+const JWTToken = 'token goes here';
 
 function App() {
+  useEffect(() => {
+
+    const setTokenData = async () => {
+      const setToken = setTokenToCookie('uniphi-token', JWTToken, 4);
+      console.log(setToken)
+    }
+    setTokenData();
+
+    const fetchTokenData = async () => {
+      const token = getTokenFromCookie('uniphi-token');
+      console.log(token)
+    }
+    fetchTokenData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
